758ea552ff7ee596607fbf3ee75395346beaa6f9
neta-r <neta2611@walla.com>
2022-02-26 18:24:56 +0200

    turned it to classes

--- Client.py
+++ Client.py
@@ -1,203 +1,191 @@
-# import socket
 import time
 import threading
 from socket import *
+from Server import Server
 
-# SERVER_ADDRESS = ('localhost', 13000)
-# TODO: figure out why we have to put specific ip instead of 'localhost'!
-serverName = '10.0.0.10'
+class Client:
+    client_socket = SocketKind
+    name = str
+    port = int
 
+    def __init__(self):
+        self.get_port()
+        while True:
+            self.name = input("Input username: ")
+            connect_request = "<connect><" + self.name + ">"
+            # clientSocket.send(bytes(sentence, encoding="UTF-8"))
+            self.client_socket.send(connect_request.encode())
+            print("Thank you!\n")
+            break
+        Client.menu(self)
+        t1 = threading.Thread(target=Client.actions, args=(self,))
+        t2 = threading.Thread(target=Client.receive_msgs, args=(self,))
+        t2.start()
+        t1.start()
 
-def get_users():
-    print(username)
-    clientSocket.send("<get_users>".encode())
-
-
-def disconnect():
-    clientSocket.send("<disconnect>".encode())
-    server_feedback = clientSocket.recv(1024).decode()
-
-
-def set_msg():
-    other_user = input("Input username: ")
-    msg = input("Input message: ")
-    set_msg_request = "<set_msg><" + other_user + "><" + msg + ">"
-    clientSocket.send(set_msg_request.encode())
-
-
-def set_msg_all():
-    msg = input("Input message: ")
-    set_msg_all_request = "<set_msg_all><" + msg + ">"
-    clientSocket.send(set_msg_all_request.encode())
-
-
-def show_all_msg():
-    clientSocket.send("<show_all_msgs>".encode())
-
-
-def get_list_file():
-    return "h"
-
-
-def download():
-    return "h"
-
-
-def proceed():
-    return "h"
-
-
-def switcher(action):
-    if action == 1:
-        get_users()
-    elif action == 2:
-        disconnect()
-    elif action == 3:
-        set_msg()
-    elif action == 4:
-        set_msg_all()
-    elif action == 5:
-        show_all_msg()
-    elif action == 6:
-        get_list_file()
-    elif action == 7:
-        download()
-    elif action == 8:
-        proceed()
-    else:
-        return "Invalid action"
-
-
-while True:
-    clientInput = input("Enter port number between 55000 to 55015: ")
-    # checking if input is a number
-    try:
-        clientPort = int(clientInput)
-        # checking if the port is out of bounds
-        if 55000 <= clientPort <= 55016:
-            # checking if specific port is available
+    def get_port(self):
+        server_name = '10.100.102.8'
+        while True:
+            client_input = input("Enter port number between 55000 to 55015: ")
+            # checking if input is a number
             try:
-                SERVER_ADDRESS = (serverName, 50000)
-                clientSocket = socket(AF_INET, SOCK_STREAM)
-                clientSocket.bind(('0.0.0.0', clientPort))
-                clientSocket.connect(SERVER_ADDRESS)
-                feedback = clientSocket.recv(1024).decode()
-                break
-            except OSError as e:
-                # error number 10048 = Port is taken,
-                # Only one usage of each socket address is normally permitted
-                if e.errno == 10048:
-                    print("Port is unavailable, Please try another one!")
+                self.port = int(client_input)
+                # checking if the port is out of bounds
+                if 55000 <= self.port <= 55016:
+                    # checking if specific port is available
+                    try:
+                        SERVER_ADDRESS = (server_name, 50000)
+                        self.client_socket = socket(AF_INET, SOCK_STREAM)
+                        self.client_socket.bind(('0.0.0.0', self.port))
+                        self.client_socket.connect(SERVER_ADDRESS)
+                        break
+                    except OSError as e:
+                        # error number 10048 = Port is taken,
+                        # Only one usage of each socket address is normally permitted
+                        if e.errno == 10048:
+                            print("Port is unavailable, Please try another one!")
+                else:
+                    print("Port out of range!\n")
+            except ValueError:
+                print("Please enter numeric value!\n")
+
+    def get_users(self):
+        self.client_socket.send("<get_users>".encode())
+
+    def disconnect(self):
+        self.client_socket.send("<disconnect>".encode())
+        exit()
+
+    def set_msg(self):
+        other_user = input("Input username: ")
+        msg = input("Input message: ")
+        set_msg_request = "<set_msg><" + other_user + "><" + msg + ">"
+        self.client_socket.send(set_msg_request.encode())
+
+    def set_msg_all(self):
+        msg = input("Input message: ")
+        set_msg_all_request = "<set_msg_all><" + msg + ">"
+        self.client_socket.send(set_msg_all_request.encode())
+
+    def show_all_msg(self):
+        self.client_socket.send("<show_all_msgs>".encode())
+
+    def get_list_file(self):
+        return "h"
+
+    def download(self):
+        return "h"
+
+    def proceed(self):
+        return "h"
+
+    def switcher(self, action):
+        if action == 1:
+            Client.get_users(self)
+        elif action == 2:
+            Client.disconnect(self)
+        elif action == 3:
+            Client.set_msg(self)
+        elif action == 4:
+            Client.set_msg_all(self)
+        elif action == 5:
+            Client.show_all_msg(self)
+        elif action == 6:
+            Client.get_list_file(self)
+        elif action == 7:
+            Client.download(self)
+        elif action == 8:
+            Client.proceed(self)
         else:
-            print("Port out of range!\n")
-    except ValueError:
-        print("Please enter numeric value!\n")
+            return "Invalid action"
 
+    def menu(self):
+        print("Action menu: \n")
+        print("1- get users list\n")
+        print("2- disconnect\n")
+        print("3- send private message\n")
+        print("4- send message to all online users\n")
+        print("5- show all messages\n")
+        print("6- get list of files\n")
+        print("7- download file\n")
+        print("8- proceed\n")
 
-def menu():
-    print("Action menu: \n")
-    print("1- get users list\n")
-    print("2- disconnect\n")
-    print("3- send private message\n")
-    print("4- send message to all online users\n")
-    print("5- show all messages\n")
-    print("6- get list of files\n")
-    print("7- download file\n")
-    print("8- proceed\n")
-
-
-def receive_msgs(clientsocket):
-
-    while True:
-        # message="<name><msg>"
-        buffer = clientSocket.recv(1024)
-        if buffer:
-            message = buffer.decode()
-
-            # get users
-            if message.startswith("<users_lst>"):
-                message = message[12:]
-                # server_feedback = "num_of_users><user1><user2>...<end>
-                index = message.find(">")
-                num_of_usr = message[0:index]
-                print("Number of users connected: " + num_of_usr + "\nThe users are: ")
-                message = message[index + 1:]
-                # server_feedback = "<usr1><usr2>...<end>
-                stringUser = ""
-                for _ in range(int(num_of_usr)):
+    def receive_msgs(self):
+        while True:
+            # message="<name><msg>"
+            buffer = self.client_socket.recv(1024)
+            if buffer:
+                message = buffer.decode()
+                # get users
+                if message.startswith("<users_lst>"):
+                    message = message[12:]
+                    # server_feedback = "num_of_users><user1><user2>...<end>
                     index = message.find(">")
-                    user = message[1:index]
-                    stringUser = stringUser + user + ", "
+                    num_of_usr = message[0:index]
+                    print("Number of users connected: " + num_of_usr + "\nThe users are: ")
                     message = message[index + 1:]
-                print(stringUser[0:-2])
+                    # server_feedback = "<usr1><usr2>...<end>
+                    stringUser = ""
+                    for _ in range(int(num_of_usr)):
+                        index = message.find(">")
+                        user = message[1:index]
+                        stringUser = stringUser + user + ", "
+                        message = message[index + 1:]
+                    print(stringUser[0:-2])
 
-            # connect
-            elif message == "<connection_established>":
-                print("connection established\n")
+                # connect
+                elif message == "<connection_established>":
+                    print("connection established\n")
 
-            # disconnect
-            elif message.startswith("<disconnected>"):
-                print("Successfully disconnected\n")
-                clientSocket.close()
-                exit()
+                # disconnect
+                elif message.startswith("<disconnected>"):
+                    print("Successfully disconnected\n")
+                    self.client_socket.close()
+                    exit()
 
-            # send message
-            elif message.startswith("<message_to_yourself>"):
-                print("You can't send a message to yourself\n")
+                # send message
+                elif message.startswith("<message_to_yourself>"):
+                    print("You can't send a message to yourself\n")
 
-            elif message.startswith("<msg_sent>"):
-                print("Message sent successfully!\n")
+                elif message.startswith("<msg_sent>"):
+                    print("Message sent successfully!\n")
 
-            elif message.startswith("<invalid_name>"):
-                print("The name you chose is not in the chatroom!\n")
+                elif message.startswith("<invalid_name>"):
+                    print("The name you chose is not in the chatroom!\n")
 
-            # show all messages
-            elif message.startswith("<no_msgs>"):
-                print("You have no messages yet\n")
+                # show all messages
+                elif message.startswith("<no_msgs>"):
+                    print("You have no messages yet\n")
 
-            elif message.startswith("<msg_lst>"):
-                message = message[10:]
-                # message = "num_of_msgs><msg1><msg2>...<end>"
-                index = message.find(">")
-                num_of_msgs = message[0:index]
-                print("Number of messages: " + num_of_msgs + "\nthe messages are:")
-                message = message[index + 1:]
-                # message = "<msg1><msg2>...<end>
-                for _ in range(int(num_of_msgs)):
+                elif message.startswith("<msg_lst>"):
+                    message = message[10:]
+                    # message = "num_of_msgs><msg1><msg2>...<end>"
                     index = message.find(">")
-                    msg = message[1:index]
-                    print(msg + "\n")
+                    num_of_msgs = message[0:index]
+                    print("Number of messages: " + num_of_msgs + "\nthe messages are:")
                     message = message[index + 1:]
+                    # message = "<msg1><msg2>...<end>
+                    for _ in range(int(num_of_msgs)):
+                        index = message.find(">")
+                        msg = message[1:index]
+                        print(msg + "\n")
+                        message = message[index + 1:]
+
+    def actions(self):
+        while True:
+            time.sleep(3)
+            client_input = input("Please select action: \n")
+            # checking if input is a number
+            try:
+                action = int(client_input)
+                if int(action) < 1 or int(action) > 8:
+                    print("Please choose number between 1 to 8\n")
+                else:
+                    Client.switcher(self, action)
+            except ValueError:
+                print("Please enter numeric value!\n")
 
 
-def actions():
-    while True:
-        time.sleep(3)
-        client_input = input("Please select action: \n")
-        # checking if input is a number
-        try:
-            action = int(client_input)
-            if int(action) < 1 or int(action) > 8:
-                print("Please choose number between 1 to 8\n")
-            else:
-                switcher(action)
-        except ValueError:
-            print("Please enter numeric value!\n")
-
-
-while True:
-    username = input("Input username: ")
-    connect_request = "<connect><" + username + ">"
-    # clientSocket.send(bytes(sentence, encoding="UTF-8"))
-    clientSocket.send(connect_request.encode())
-    print("Thank you!\n")
-    break
-menu()
-t1 = threading.Thread(target=actions)
-t2 = threading.Thread(target=receive_msgs, args=(clientSocket,))
-t2.start()
-t1.start()
-
-t2.join()
-
+if __name__ == '__main__':
+    client = Client()
 
--- Server.py
+++ Server.py
@@ -1,182 +1,168 @@
-import threading
-import time
 from _thread import start_new_thread
-# import socket module
 from socket import *
 import sys  # In order to terminate the program
-num_of_threads = 0
-
-# client's Port: user's name
-names = {55000: str, 55001: str, 55002: str, 55003: str, 55004: str, 55005: str,
-         55006: str, 55007: str, 55008: str, 55009: str, 55010: str, 55011: str,
-         55012: str, 55013: str, 55014: str, 55015: str}
-
-# name: [ip, port, connection, "neta:hi, reut:hi, neta:how you doing, ... "]
-users = {}
 
 
-# connect to chat! In this point we already have a connection to the server
-def connect(name, port, ip):
-    if name not in users:
-        names[port] = name
-        users[name] = [ip, port, connectionSocket, ""]
-        connectionSocket.send('<connected>'.encode())
-        print(name + " connected")
-    else:
-        connectionSocket.send('<available_name>'.encode())
+class Server:
+    # client's Port: user's name
+    names = {55000: str, 55001: str, 55002: str, 55003: str, 55004: str, 55005: str,
+             55006: str, 55007: str, 55008: str, 55009: str, 55010: str, 55011: str,
+             55012: str, 55013: str, 55014: str, 55015: str}
+
+    # name: [ip, port, connection, "neta:hi, reut:hi, neta:how you doing, ... "]
+    users = {}
+
+    online_users = 0
+
+    def __init__(self):
+        # Given server port
+        server_port = 50000
+        # choosing type of protocol
+        # af_inet- Ivp4
+        # SOCK_STREAM = TCP
+        server_socket = socket(AF_INET, SOCK_STREAM)
+
+        # bind socket to a specific address and port
+        # '' means listen to all ip's
+
+        server_socket.bind(('', server_port))
+
+        # define at least 5 connections
+        server_socket.listen(5)
+
+        print("Ready to serve!")
+
+        while True:
+            # connectionSocket is the socket after the connection has been accepted
+            # addr[0]= client's ip, addr[1]= client's port
+            connection_socket, addr = server_socket.accept()
+            connection_socket.send("<connection_established>".encode())
+            Server.online_users = int(Server.online_users)+1
+            start_new_thread(Server.multi_threaded_client, (self, connection_socket, addr[0], addr[1]))
+
+    # this function is executed whenever a thread is being activated
+    def multi_threaded_client(self, connection_socket, ip, port):
+        while True:
+            # receiving other messages
+            message = connection_socket.recv(1024).decode()
+            index = message.find(">")
+            action = message[1:index]
+            # sending to a switch case action and other relevant info
+            Server.actions(self, action, message[index + 2:-1], ip, port, connection_socket)
+            if action == "disconnect":
+                break
+        connection_socket.close()
+
+    # connect to chat! In this point we already have a connection to the server
+    def connect(self, name, port, ip, connection_socket):
+        if name not in Server.users:
+            Server.names[port] = name
+            Server.users[name] = [ip, port, connection_socket, ""]
+            connection_socket.send('<connected>'.encode())
+            print(name + " connected")
+        else:
+            connection_socket.send('<available_name>'.encode())
+
+    # message_send- is the massage according to protocol
+    # online_users_print- will display on the screen
+    def get_users(self, connection_socket):
+        online_users_print = ""
+        online_users_send = ""
+        for key, val in Server.users.items():
+            if val[2] != 0:
+                online_users_print = online_users_print + str(key) + ","
+                online_users_send = online_users_send + "<" + str(key) + ">"
+
+        print("online users: " + online_users_print[0:-1])
+        message_send = "<users_lst><" + str(Server.online_users) + ">" + str(online_users_send) + "<end>"
+        connection_socket.send(message_send.encode())
+
+    # we need to free the port
+    def disconnect(self, port, connection_socket):
+        name = Server.names[port]
+        Server.users.pop(Server.names[port])
+        # del users[names[port]]
+        Server.names[port] = str
+        connection_socket.send("<disconnected>".encode())
+        print(name + ' left chat')
+
+    def set_msg(self, rest_of_msg, port, ip, connection_socket):
+        my_name = Server.names[port]
+        # rest_of_msg="name><msg>"
+        index = rest_of_msg.find(">")
+        name = rest_of_msg[0:index]
+        msg = rest_of_msg[index + 2:]
+        # if name == my_name:
+        #     connectionSocket.send("<message_to_yourself>".encode())
+        # else:
+        if name in Server.users:
+            other_client_socket = Server.users[name][2]
+            send_to_client = "<" + str(my_name) + "><" + str(msg) + ">"
+            other_client_socket.send(send_to_client.encode())
+            Server.users[name][3] += my_name + ":" + msg + ","
+            connection_socket.send("<msg_sent>".encode())
+        else:
+            connection_socket.send("<invalid_name>".encode())
+
+    def set_msg_all(self, msg, port, connection_socket):
+        # msg = "msg>"
+        my_name = Server.names[port]
+        for key, val in Server.users.items():
+            # if key is not my_name:
+            other_client_socket = Server.users[key][2]
+            Server.users[key][3] += my_name + ":" + msg + ","
+            other_client_socket.send(msg.encode())
+        connection_socket.send("<msg_sent>".encode())
+
+    def show_all_msg(self, port, connection_socket):
+        # TODO: לשנות את ההפרדה לדיקשנרי
+        # TODO: לוודא ביצוע פעולות עבור משתמשים שהתחברו מאוחר יותר (טרדים)
+        my_name = Server.names[port]
+        msgs = Server.users[my_name][3]
+        if msgs != "":
+            msgs_and_users = msgs.split(',')
+            num_of_msgs = len(msgs_and_users) - 1
+            send = "<msg_lst><" + str(num_of_msgs) + ">"
+            for arr in msgs_and_users:
+                if arr != "":
+                    user_name, user_msg = arr.split(':')
+                    send += "<" + user_name + ":" + user_msg + ">"
+            send += "<end>"
+            connection_socket.send(send.encode())
+        else:
+            connection_socket.send("<no_msgs>".encode())
+
+    def get_list_file(self, connection_socket):
+        return "h"
+
+    def download(self, connection_socket):
+        return "h"
+
+    def proceed(self, connection_socket):
+        return "h"
+
+    def actions(self, action, rest_of_msg, port, ip, connection_socket):
+        if action == "connect":
+            Server.connect(self, rest_of_msg, port, ip, connection_socket)
+        elif action == "get_users":
+            Server.get_users(self, connection_socket)
+        elif action == "disconnect":
+            Server.disconnect(self, port, connection_socket)
+        elif action == "set_msg":
+            Server.set_msg(self, rest_of_msg, port, ip, connection_socket)
+        elif action == "set_msg_all":
+            Server.set_msg_all(self, rest_of_msg, port, connection_socket)
+        elif action == "show_all_msgs":
+            Server.show_all_msg(self, port, connection_socket)
+        elif action == "get_list_file":
+            Server.get_list_file(self, connection_socket)
+        elif action == "download":
+            Server.download(self, connection_socket)
+        elif action == "proceed":
+            Server.proceed(self, connection_socket)
+        else:
+            return "Invalid action"
 
 
-# message_send- is the massage according to protocol
-# online_users_print- will display on the screen
-def get_users():
-    online_users_print = ""
-    online_users_send = ""
-    for key, val in users.items():
-        if val[2] != 0:
-            online_users_print = online_users_print + str(key) + ","
-            online_users_send = online_users_send + "<" + str(key) + ">"
-
-    print("online users: " + online_users_print[0:-1])
-    message_send = "<users_lst><" + str(num_of_threads) + ">" + str(online_users_send) + "<end>"
-    print("insideeeeeee: " + str(connectionSocket))
-    connectionSocket.send(message_send.encode())
-
-
-# we need to free the port
-def disconnect(port):
-    name = names[port]
-    users.pop(names[port])
-    # del users[names[port]]
-    names[port] = str
-    connectionSocket.send("<disconnected>".encode())
-    print(name + ' left chat')
-    print("Num of threads is: " + str(num_of_threads-1))
-
-
-def set_msg(rest_of_msg, port, ip):
-    my_name = names[port]
-    # rest_of_msg="name><msg>"
-    index = rest_of_msg.find(">")
-    name = rest_of_msg[0:index]
-    msg = rest_of_msg[index + 2:]
-    # if name == my_name:
-    #     connectionSocket.send("<message_to_yourself>".encode())
-    # else:
-    if name in users:
-        other_client_socket = users[name][2]
-        send_to_client = "<" + str(my_name) + "><" + str(msg) + ">"
-        other_client_socket.send(send_to_client.encode())
-        users[name][3] += my_name + ":" + msg + ","
-        connectionSocket.send("<msg_sent>".encode())
-    else:
-        print("Don't know " + name)
-        connectionSocket.send("<invalid_name>".encode())
-
-
-def set_msg_all(msg, port):
-    # msg = "msg>"
-    my_name = names[port]
-    for key, val in users.items():
-        # if key is not my_name:
-        other_client_socket = users[key][2]
-        users[key][3] += my_name + ":" + msg + ","
-        other_client_socket.send(msg.encode())
-    connectionSocket.send("<msg_sent>".encode())
-
-
-def show_all_msg(port):
-    # TODO: לשנות את ההפרדה לדיקשנרי
-    # TODO: לוודא ביצוע פעולות עבור משתמשים שהתחברו מאוחר יותר (טרדים)
-    my_name = names[port]
-    msgs = users[my_name][3]
-    if msgs != "":
-        msgs_and_users = msgs.split(',')
-        num_of_msgs = len(msgs_and_users) - 1
-        send = "<msg_lst><" + str(num_of_msgs) + ">"
-        for arr in msgs_and_users:
-            if arr != "":
-                user_name, user_msg = arr.split(':')
-                send += "<" + user_name + ":" + user_msg + ">"
-        send += "<end>"
-        connectionSocket.send(send.encode())
-    else:
-        connectionSocket.send("<no_msgs>".encode())
-
-
-def get_list_file():
-    return "h"
-
-
-def download():
-    return "h"
-
-
-def proceed():
-    return "h"
-
-
-def actions(action, rest_of_msg, port, ip):
-    if action == "connect":
-        connect(rest_of_msg, port, ip)
-    elif action == "get_users":
-        print(connectionSocket)
-        get_users()
-    elif action == "disconnect":
-        disconnect(port)
-    elif action == "set_msg":
-        set_msg(rest_of_msg, port, ip)
-    elif action == "set_msg_all":
-        set_msg_all(rest_of_msg, port)
-    elif action == "show_all_msgs":
-        show_all_msg(port)
-    elif action == "get_list_file":
-        get_list_file()
-    elif action == "download":
-        download()
-    elif action == "proceed":
-        proceed()
-    else:
-        return "Invalid action"
-
-
-# Given server port
-serverPort = 50000
-# choosing type of protocol
-# af_inet- Ivp4
-# SOCK_STREAM = TCP
-serverSocket = socket(AF_INET, SOCK_STREAM)
-
-# bind socket to a specific address and port
-# '' means listen to all ip's
-serverSocket.bind(('', serverPort))
-
-# define at least 5 connections
-serverSocket.listen(5)
-
-print("Ready to serve!")
-
-
-# this function is executed whenever a thread is being activated
-def multi_threaded_client(connectionSocket):
-    print("num of threads is: " + str(num_of_threads))
-    while True:
-        # receiving other messages
-        message = connectionSocket.recv(1024).decode()
-        index = message.find(">")
-        action = message[1:index]
-        # sending to a switch case action and other relevant info
-        actions(action, message[index + 2:-1], addr[1], addr[0])
-        if action == "disconnect":
-            break
-    connectionSocket.close()
-
-
-while True:
-    # connectionSocket is the socket after the connection has been accepted
-    # addr[0]= client's ip, addr[1]= client's port
-    connectionSocket, addr = serverSocket.accept()
-    connectionSocket.send("<connection_established>".encode())
-    start_new_thread(multi_threaded_client, (connectionSocket,))
-
-
-    num_of_threads += 1
+if __name__ == '__main__':
+    server = Server()
